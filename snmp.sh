ed8d19b92820dc7ed66b74367abed80ad950295d
JeremyLardenois <jeremy.lardenois@hotmail.fr>
2022-02-03 17:35:34 +0100

    Shellcheck compilant

--- snmp.sh
+++ snmp.sh
@@ -17,21 +17,21 @@ if [ "${8}" = "" ]; then usage; fi
 ARGS=$(echo "$@" |sed -e 's:-[a-Z] :\n&:g' | sed -e 's: ::g')
 
 for i in $ARGS; do
-        if [ -n "$(echo ${i} | grep "^\-C")" ]; then COMMUNITY=$(echo ${i} | cut -c 3-); if [ ! -n "${COMMUNITY}" ]; then usage;fi;fi
-        if [ -n "$(echo ${i} | grep "^\-H")" ]; then HOSTTARGET=$(echo ${i} | cut -c 3-); if [ ! -n "${HOSTTARGET}" ]; then usage;fi;fi
-        if [ -n "$(echo ${i} | grep "^\-w")" ]; then WARNING=$(echo ${i} | cut -c 3-); if [ ! -n "${WARNING}" ]; then usage;fi;fi
-        if [ -n "$(echo ${i} | grep "^\-c")" ]; then CRITICAL=$(echo ${i} | cut -c 3-); if [ ! -n "${CRITICAL}" ]; then usage;fi;fi
+        if echo "${i}" | grep -q "^\-C"; then COMMUNITY=$(echo "${i}" | cut -c 3-); if [ -z "${COMMUNITY}" ]; then usage;fi;fi
+        if echo "${i}" | grep -q "^\-H"; then HOSTTARGET=$(echo "${i}" | cut -c 3-); if [ -z "${HOSTTARGET}" ]; then usage;fi;fi
+        if echo "${i}" | grep -q "^\-w"; then WARNING=$(echo "${i}" | cut -c 3-); if [ -z "${WARNING}" ]; then usage;fi;fi
+        if echo "${i}" | grep -q "^\-c"; then CRITICAL=$(echo "${i}" | cut -c 3-); if [ -z "${CRITICAL}" ]; then usage;fi;fi
 done
 
 TMPDIR="/tmp/tmp-check_ntp_nb_client/${HOSTTARGET}"
 # If the directory does not exist, create it
-if [ ! -d "${TMPDIR}" ]; then mkdir -p ${TMPDIR};fi
+if [ ! -d "${TMPDIR}" ]; then mkdir -p "${TMPDIR}";fi
 
 # If file does exist
-if [ -f $TMPDIR/check_ntp_nb_client_out.txt ]
+if [ -f "$TMPDIR/check_ntp_nb_client_out.txt" ]
 then 
-	previous=$(cat $TMPDIR/check_ntp_nb_client_out.txt)
-	check_actual=$(snmpwalk -v 2c -c $COMMUNITY $HOSTTARGET -O 0qv 1.3.6.1.4.1.8955.1.8.2.3.0 | sed -e 's: "$:":g')
+	previous=$(cat "$TMPDIR/check_ntp_nb_client_out.txt")
+	check_actual=$(snmpwalk -v 2c -c "$COMMUNITY" "$HOSTTARGET" -O 0qv 1.3.6.1.4.1.8955.1.8.2.3.0 | sed -e 's: "$:":g')
 	delta=$((check_actual-previous))
 	if [ "$delta" -lt "$CRITICAL" ]
 	then
